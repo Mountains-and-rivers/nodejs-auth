f3e242d3143321f8c26d2b1688a706568024487f
Mountains-and-rivers <sjdykp@163.com>
2019-09-30 18:35:39 +0800

    1

---
+++ aclDemo/config/env/development.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port: 3008,
+  mongo: "mongodb://root:adminpassword@127.0.0.1:27017/admin"
+};
---
+++ node_passport_login/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://root:adminpassword@127.0.0.1:27017/admin"
+};
